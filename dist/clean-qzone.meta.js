2834d148ced16acb2e76108c463aac4a5d5328b9
Saiya <evecalm@live.com>
2017-11-04 23:13:23 +0800

    fixed bugs

--- dist/clean-qzone.meta.js
+++ dist/clean-qzone.meta.js
@@ -3,9 +3,9 @@
 // @namespace      com.saiya.clean-qzone
 // @author         Saiya
 // @description    Remove all Qzone ads, make your Qzone center clean and clear.
-// @match          http://*.qzone.qq.com/*
-// @include        http://*.qzone.qq.com/*
+// @match          *://*.qzone.qq.com/*
+// @include        *://*.qzone.qq.com/*
 // @updateURL      https://http://app.evecalm.com/clean-qzone/dist/clean-qzone.meta.js
 // @downloadURL    https://http://app.evecalm.com/clean-qzone/dist/clean-qzone.user.js
-// @version        0.1.10
+// @version        0.1.11
 // ==/UserScript==
