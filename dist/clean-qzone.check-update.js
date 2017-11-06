b46acd177da26a9826a52d4da73669d55aa13054
Saiya <evecalm@live.com>
2017-11-06 09:45:15 +0800

    修正检查更新路径错误的问题

--- dist/clean-qzone.check-update.js
+++ dist/clean-qzone.check-update.js
@@ -1,1 +1,1 @@
-checkCqUpdate({'version': '0.1.11', 'updateMsg': '修正在https协议下不可使用的问题'})
+checkCqUpdate({'version': '0.1.12', 'updateMsg': '修正在https协议下不可使用的问题'})
--- dist/clean-qzone.meta.js
+++ dist/clean-qzone.meta.js
@@ -5,7 +5,7 @@
 // @description    Remove all Qzone ads, make your Qzone center clean and clear.
 // @match          *://*.qzone.qq.com/*
 // @include        *://*.qzone.qq.com/*
-// @updateURL      https://http://app.evecalm.com/clean-qzone/dist/clean-qzone.meta.js
-// @downloadURL    https://http://app.evecalm.com/clean-qzone/dist/clean-qzone.user.js
-// @version        0.1.11
+// @updateURL      http://app.evecalm.com/clean-qzone/dist/clean-qzone.meta.js
+// @downloadURL    http://app.evecalm.com/clean-qzone/dist/clean-qzone.user.js
+// @version        0.1.12
 // ==/UserScript==
